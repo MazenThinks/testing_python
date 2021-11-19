547b090ebab2c7b403a8c688d802764c0f66939c
Mazen Magdy <mazenmagdy100.mm@gmail.com>
2021-11-19 22:04:18 +0200

    Looped through list

--- src/my_math.py
+++ src/my_math.py
@@ -3,13 +3,21 @@ x = 5
 
 y = 6   
 
-print( str(x) + str(y))
+#print( str(x) + str(y))
 
 
-print(x + y)
+#print(x + y)
+
+my_list = [1,2,3,4,5,6,7,8,9]
+
+for item in my_list:
+    print(item)
 
 
+name = "Mazen"
 
+for char in name:
+    print(name)
 
 
 
